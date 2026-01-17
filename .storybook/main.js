b4274e97b7c11cef5c85febd270bc49a8cdcfe6b
Meghea Iulian <iulian@plumelo.com>
2026-01-17 16:36:12 +0200

    chore: upgrade storybook to v10
    - Switch from @web/storybook-* (Rollup) to @storybook/web-components-vite
    - Remove @storybook/addon-essentials (now built into storybook core)
    - Remove rollup-plugin-esbuild (no longer needed with Vite)
    - Add "type": "module" to package.json
    - Update preview.js to v10 format

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,5 @@
 export default {
 	stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-	framework: {
-		name: '@web/storybook-framework-web-components',
-	},
-	core: {
-		builder: {
-			name: '@web/storybook-builder',
-		},
-	},
+	addons: ['@storybook/addon-links'],
+	framework: '@storybook/web-components-vite',
 };
