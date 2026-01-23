d2cede698420c8e2adc48f92ed232b7d1452ccf7
Cristian Necula <virusz@gmail.com>
2026-01-23 14:01:23 +0200

    feat: export all modules from main index
    This change exports all queue modules from the main index file, eliminating
    the need for subpath imports like '@neovici/cosmoz-queue/item-click.js'.
    
    Previously, consumers had to use subpath imports:
      import { itemClick } from '@neovici/cosmoz-queue/item-click.js';
      import { usePagination } from '@neovici/cosmoz-queue/pagination.js';
    
    Now everything can be imported from the main entry:
      import { itemClick, usePagination } from '@neovici/cosmoz-queue';
    
    This is needed because:
    1. The package.json 'exports' field only exposes a few subpaths, causing
       TypeScript resolution issues with 'moduleResolution: bundler'
    2. Consumers (like cosmoz-frontend) had to add tsconfig path mappings as
       workarounds to resolve the unexported subpaths
    3. Having all exports in the main index improves discoverability and
       simplifies imports
    
    New exports added:
    - itemClick (from item-click)
    - omnitable (from omnitable)
    - usePagination (from pagination)
    - queue (from queue)
    - base, renderStyles (from style)
    - useFetchActions, actions$ (from use-fetch-actions)
    - useListSSE (from use-list-sse)
    - getItems (from util)
    - useDataNav (from use-data-nav)
    - useSplit (from use-split)
    - useTabs (from use-tabs)
    - useList, useListState, filter, notify (from use-list)

--- src/queue/index.ts
+++ src/queue/index.ts
@@ -2,9 +2,39 @@ export { default as useQueue } from './use-queue';
 export { default as useDataService } from './use-data-service';
 export { default as useAsyncAction } from './use-async-action';
 
-export { touch } from './util';
+export { touch, getItems } from './util';
 
 export * from './render';
 
 export type * from './types';
 export type * from './use-queue';
+
+// Item interactions
+export { itemClick } from './item-click';
+
+// Omnitable integration
+export { default as omnitable } from './omnitable';
+
+// Pagination
+export { usePagination } from './pagination';
+
+// Queue core
+export { queue } from './queue';
+
+// Styling
+export { base, default as renderStyles } from './style';
+
+// Fetch actions
+export { default as useFetchActions, actions$ } from './use-fetch-actions';
+export type { Item, Props } from './use-fetch-actions';
+
+// SSE
+export { useListSSE } from './use-list-sse';
+
+// Navigation & layout hooks
+export { default as useDataNav } from './use-data-nav';
+export { default as useSplit } from './use-split';
+export { default as useTabs } from './use-tabs';
+
+// List hooks
+export { useList, useListState, filter, notify } from './use-list';
